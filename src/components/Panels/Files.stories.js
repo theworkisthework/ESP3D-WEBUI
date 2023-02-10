3a4da4e072b6a9b41df879097c40b94804a21804
Mark Benson <mark@sub-design.co.uk>
2023-02-10 21:25:28 +0000

    Try and get storybook working

---
+++ src/components/Panels/Files.stories.js
@@ -0,0 +1,14 @@
+// Button.stories.js|jsx
+
+import React from 'react';
+
+import { Files } from './Files';
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'Panels/Fails',
+  component: Files,
+};
